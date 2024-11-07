b40f15b1b28846f5a3b1893ac6f70d17cfd3ee06
VandanSonani <vsona2@uic.edu>
2024-11-07 17:46:41 -0600

    Fixed tests

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,12 +1,8 @@
-import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-
-import org.junit.jupiter.api.DisplayName;
-
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class MyTest {
 
@@ -33,7 +29,7 @@ class MyTest {
 		assertEquals(13, kingSpades.getValue());
 		assertEquals('C', twoClubs.getSuit());
 		assertEquals(2, twoClubs.getValue());
-		assertEquals("Hearts", aceHearts.getSuitAsString());
+		assertEquals("hearts", aceHearts.getSuitAsString());
 	}
 
 	@Test
